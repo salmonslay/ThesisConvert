2f9fefe044e16afd91ffaeb7b3d731baf043e7b4
Sofia Lindgren <hi@sofia.kiwi>
2024-03-11 16:03:07 +0100

    Write the program lol

---
+++ Html.cs
@@ -0,0 +1,34 @@
+﻿namespace ThesisConvert;
+
+public class Html
+{
+    private string _html = "";
+
+    public void AddText(string author, string text)
+    {
+        if (text != "Image")
+            _html += $"<p><b>{author}</b>: {Escape(text)}</p>";
+        else
+            // highlight images to make them easier to find (& add)
+            _html += $"<p style='background-color: salmon;'><b>{author}</b>: {Escape(text)}</p>";
+    }
+
+    public void AddHeader(string text)
+    {
+        _html += $"<h1>{Escape(text)}</h1>";
+    }
+
+    public string GetHtml()
+    {
+        return _html;
+    }
+
+    public static string Escape(string s)
+    {
+        return s.Replace("&", "&amp;")
+            .Replace("<", "&lt;")
+            .Replace(">", "&gt;")
+            .Replace("\"", "&quot;")
+            .Replace("'", "&apos;");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,104 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace ThesisConvert
 {
+    internal enum LineAuthor
+    {
+        Unknown,
+        Interviewer,
+        Interviewee,
+    };
+
     internal class Program
     {
-        static void Main(string[] args)
+        private string _interviewer = "";
+        private string _interviewee = "";
+        private string _pseudonym = "";
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isRunning = true;
+            while (isRunning)
+                isRunning = new Program().Run();
+        }
+
+        private bool Run()
+        {
+            // Get interviewer
+            Console.Write("Username of interviewer: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            _interviewer = Console.ReadLine() ?? "Salmon";
+            Console.ResetColor();
+
+            // Get interviewee
+            Console.Write("Username of interviewee: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            _interviewee = Console.ReadLine() ?? "Bruh-mannen";
+            Console.ResetColor();
+
+            Console.Write("Pseudonym for interviewee: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            _pseudonym = Console.ReadLine() ?? "P3";
+            Console.ResetColor();
+
+            Console.WriteLine($"{_interviewer} is interviewing {_interviewee}.");
+            Html html = new Html();
+            html.AddHeader($"{_interviewer} is interviewing {_interviewee}.");
+
+            // Get transcription path
+            Console.Write("Path to transcript: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string path = Console.ReadLine() ?? "transcript.txt";
+            Console.ResetColor();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+                LineAuthor lastAuthor = LineAuthor.Unknown;
+                int i = 0;
+                foreach (string line in lines)
+                {
+                    // Get new line author
+                    LineAuthor author = GetLineAuthor(line);
+                    if (author == LineAuthor.Unknown)
+                    {
+                        html.AddText(lastAuthor == LineAuthor.Interviewer ? "I" : _pseudonym, line);
+                        i++;
+                    }
+
+                    if (author != LineAuthor.Unknown)
+                        lastAuthor = author;
+                }
+
+
+                // Write the transcript to a file
+                string fileName = $"{_pseudonym}.html";
+                Console.WriteLine($"Transcribed {i} lines to {fileName}. Opening...");
+
+                File.WriteAllText(fileName, html.GetHtml());
+                Process.Start(@"cmd.exe ", @"/c " + fileName);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e + "\n\nRestarting...\n");
+                Console.ResetColor();
+
+                return true;
+            }
+
+            return true;
+        }
+
+        private LineAuthor GetLineAuthor(string line)
+        {
+            if (new Regex($@"^{Regex.Escape(_interviewer)} — .+(?:\d\d) ?(?:(?:P|A)M)?$").IsMatch(line))
+                return LineAuthor.Interviewer;
+            if (new Regex($@"^{Regex.Escape(_interviewee)} — .+(?:\d\d) ?(?:(?:P|A)M)?$").IsMatch(line))
+                return LineAuthor.Interviewee;
+
+            return LineAuthor.Unknown;
         }
     }
 }
