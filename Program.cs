3e97dc30d6b291257cb494a3f22c2a49c4fc6b45
Sofia Lindgren <hi@sofia.kiwi>
2024-03-12 10:34:15 +0100

    Use file scoped namespaces

--- Program.cs
+++ Program.cs
@@ -1,104 +1,103 @@
 ﻿using System.Diagnostics;
 using System.Text.RegularExpressions;
 
-namespace ThesisConvert
+namespace ThesisConvert;
+
+internal enum LineAuthor
 {
-    internal enum LineAuthor
+    Unknown,
+    Interviewer,
+    Interviewee,
+};
+
+internal class Program
+{
+    private string _interviewer = "";
+    private string _interviewee = "";
+    private string _pseudonym = "";
+
+    private static void Main(string[] args)
     {
-        Unknown,
-        Interviewer,
-        Interviewee,
-    };
+        bool isRunning = true;
+        while (isRunning)
+            isRunning = new Program().Run();
+    }
 
-    internal class Program
+    private bool Run()
     {
-        private string _interviewer = "";
-        private string _interviewee = "";
-        private string _pseudonym = "";
+        // Get interviewer
+        Console.Write("Username of interviewer: ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        _interviewer = Console.ReadLine() ?? "Salmon";
+        Console.ResetColor();
 
-        private static void Main(string[] args)
+        // Get interviewee
+        Console.Write("Username of interviewee: ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        _interviewee = Console.ReadLine() ?? "Bruh-mannen";
+        Console.ResetColor();
+
+        Console.Write("Pseudonym for interviewee: ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        _pseudonym = Console.ReadLine() ?? "P3";
+        Console.ResetColor();
+
+        Console.WriteLine($"{_interviewer} is interviewing {_interviewee}.");
+        Html html = new Html();
+        html.AddHeader($"{_interviewer} is interviewing {_interviewee}.");
+
+        // Get transcription path
+        Console.Write("Path to transcript: ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        string path = Console.ReadLine() ?? "transcript.txt";
+        Console.ResetColor();
+
+        try
         {
-            bool isRunning = true;
-            while (isRunning)
-                isRunning = new Program().Run();
-        }
-
-        private bool Run()
-        {
-            // Get interviewer
-            Console.Write("Username of interviewer: ");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            _interviewer = Console.ReadLine() ?? "Salmon";
-            Console.ResetColor();
-
-            // Get interviewee
-            Console.Write("Username of interviewee: ");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            _interviewee = Console.ReadLine() ?? "Bruh-mannen";
-            Console.ResetColor();
-
-            Console.Write("Pseudonym for interviewee: ");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            _pseudonym = Console.ReadLine() ?? "P3";
-            Console.ResetColor();
-
-            Console.WriteLine($"{_interviewer} is interviewing {_interviewee}.");
-            Html html = new Html();
-            html.AddHeader($"{_interviewer} is interviewing {_interviewee}.");
-
-            // Get transcription path
-            Console.Write("Path to transcript: ");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            string path = Console.ReadLine() ?? "transcript.txt";
-            Console.ResetColor();
-
-            try
+            string[] lines = File.ReadAllLines(path);
+            LineAuthor lastAuthor = LineAuthor.Unknown;
+            int i = 0;
+            foreach (string line in lines)
             {
-                string[] lines = File.ReadAllLines(path);
-                LineAuthor lastAuthor = LineAuthor.Unknown;
-                int i = 0;
-                foreach (string line in lines)
+                // Get new line author
+                LineAuthor author = GetLineAuthor(line);
+                if (author == LineAuthor.Unknown)
                 {
-                    // Get new line author
-                    LineAuthor author = GetLineAuthor(line);
-                    if (author == LineAuthor.Unknown)
-                    {
-                        html.AddText(lastAuthor == LineAuthor.Interviewer ? "I" : _pseudonym, line);
-                        i++;
-                    }
-
-                    if (author != LineAuthor.Unknown) // Only update last if we have a new author
-                        lastAuthor = author;
+                    html.AddText(lastAuthor == LineAuthor.Interviewer ? "I" : _pseudonym, line);
+                    i++;
                 }
-                
-                // Write the transcript to a file
-                string fileName = $"{Path.GetTempPath()}{_pseudonym}.html";
-                Console.WriteLine($"Transcribed {i} lines to {fileName}. Opening...");
 
-                File.WriteAllText(fileName, html.GetHtml());
-                Process.Start(@"cmd.exe ", @"/c " + fileName);
+                if (author != LineAuthor.Unknown) // Only update last if we have a new author
+                    lastAuthor = author;
             }
-            catch (Exception e)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.ResetColor();
-                Console.WriteLine(e + "\n\nRestarting...\n");
 
-                return true;
-            }
+            // Write the transcript to a file
+            string fileName = $"{Path.GetTempPath()}{_pseudonym}.html";
+            Console.WriteLine($"Transcribed {i} lines to {fileName}. Opening...");
+
+            File.WriteAllText(fileName, html.GetHtml());
+            Process.Start(@"cmd.exe ", @"/c " + fileName);
+        }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ResetColor();
+            Console.WriteLine(e + "\n\nRestarting...\n");
 
             return true;
         }
 
-        private LineAuthor GetLineAuthor(string line)
-        {
-            // This regex matches Discord message headers (e.g. "Salmon — 12:34 PM"). PM/AM is optional.
-            if (new Regex($@"^{Regex.Escape(_interviewer)} — .+(?:\d\d) ?(?:(?:P|A)M)?$").IsMatch(line))
-                return LineAuthor.Interviewer;
-            if (new Regex($@"^{Regex.Escape(_interviewee)} — .+(?:\d\d) ?(?:(?:P|A)M)?$").IsMatch(line))
-                return LineAuthor.Interviewee;
+        return true;
+    }
 
-            return LineAuthor.Unknown;
-        }
+    private LineAuthor GetLineAuthor(string line)
+    {
+        // This regex matches Discord message headers (e.g. "Salmon — 12:34 PM"). PM/AM is optional.
+        if (new Regex($@"^{Regex.Escape(_interviewer)} — .+(?:\d\d) ?(?:(?:P|A)M)?$").IsMatch(line))
+            return LineAuthor.Interviewer;
+        if (new Regex($@"^{Regex.Escape(_interviewee)} — .+(?:\d\d) ?(?:(?:P|A)M)?$").IsMatch(line))
+            return LineAuthor.Interviewee;
+
+        return LineAuthor.Unknown;
     }
 }
